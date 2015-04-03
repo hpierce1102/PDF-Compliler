245dd98ad929c6d4bc4957f2de1a84c7cec997b1
Hayden Pierce <hpierce1102@gmail.com>
2015-04-03 13:45:46 -0500

    Dynamically generates input sections.

--- main.py
+++ main.py
@@ -11,47 +11,96 @@ class PDFCombiner:
         master.title('PDF Combiner')
         master.resizable(False, False)    
 
+        #Stores an index for all generated inputs
+        #self.inputs = [0]
+        self.chooseButtons = {}
+        self.chooseLabelVars = {}
+        self.chooseLabels = {}
+
         self.frame_header = ttk.Frame(master)
         self.frame_header.pack()
 
-        self.PDF1path= StringVar()
-        self.PDF1label = ttk.Label(self.frame_header,
-        	textvariable = self.PDF1path
-        	).grid(row = 0, column = 1)
-        self.PDF1button = ttk.Button(self.frame_header,
-        	text = "Choose PDF",
-        	command = lambda: self.open(self.PDF1path)
-        	).grid(row = 0, column = 0)
-        self.PDF1path.set("unset")
+        self.clear_button = ttk.Button(self.frame_header, text = "Clear")
+        self.clear_button.grid(row = 1, column = 0)
+        self.combine_button = ttk.Button(self.frame_header, text = "Combine")
+        self.combine_button.grid(row = 1, column = 1)
 
-        self.PDF2path = StringVar()
-        self.PDF2label = ttk.Label(self.frame_header, 
-        	textvariable = self.PDF2path
-        	).grid(row = 1, column = 1)
-        self.PDF2button = ttk.Button(self.frame_header,
-        	text = "Choose PDF",
-        	command = lambda: self.open(self.PDF2path)
-        	).grid(row = 1, column = 0)
-        self.PDF2path.set("unset")
+        self.generate_choose(0)
 
-        self.output = StringVar()
-        self.outputlabel = ttk.Label(self.frame_header,
-            textvariable = self.output
-            ).grid(row = 2, column = 1)
-        self.outputbutton = ttk.Button(self.frame_header,
-            text = "Choose Output",
-            command = lambda: self.open(self.output)
-            ).grid(row = 2, column = 0)
-        self.output.set("unset")
 
-        self.clear= ttk.Button(self.frame_header, text = "Clear").grid(row = 3, column = 0)
-        self.combine = ttk.Button(self.frame_header, text = "Combine").grid(row = 3, column = 1)
+
+        #self.PDF1path= StringVar()
+        #self.PDF1label = ttk.Label(self.frame_header,
+        #	textvariable = self.PDF1path
+        #	).grid(row = 0, column = 1)
+        #self.PDF1button = ttk.Button(self.frame_header,
+        #	text = "Choose PDF",
+        #	command = lambda: self.open(self.PDF1path)
+        #	).grid(row = 0, column = 0)
+        #self.PDF1path.set("unset")
+
+        #self.PDF2path = StringVar()
+        #self.PDF2label = ttk.Label(self.frame_header, 
+        #	textvariable = self.PDF2path
+        #	).grid(row = 1, column = 1)
+        #self.PDF2button = ttk.Button(self.frame_header,
+        #	text = "Choose PDF",
+        #	command = lambda: self.open(self.PDF2path)
+        #	).grid(row = 1, column = 0)
+        #self.PDF2path.set("unset")
+
+
+        #self.output = StringVar()
+        #self.outputlabel = ttk.Label(self.frame_header,
+        #    textvariable = self.output
+        #    ).grid(row = 2, column = 1)
+        #self.outputbutton = ttk.Button(self.frame_header,
+        #    text = "Choose Output",
+        #    command = lambda: self.open(self.output)
+        #    ).grid(row = 2, column = 0)
+        #self.output.set("unset")
+
+        
 
         self.frame_content = ttk.Frame(master)
         self.frame_content.pack()
 
-    def open(label, self):
-        self.set(filedialog.askopenfilename())
+        
+        #print(self.clear_button)
+
+    def open(self, label):
+        label.set(filedialog.askopenfilename())
+        if label.get():
+            index = len(self.chooseLabels)
+            #self.inputs.append(index)
+            self.generate_choose(index)
+
+    def clear():
+        #self.
+        pass        
+
+    def generate_choose(self, ele):
+        #x = 0
+        #for i in self.inputs:
+            self.chooseLabelVars[ele] = StringVar()
+            self.chooseLabels[ele] = ttk.Label(self.frame_header,
+                textvariable = self.chooseLabelVars[ele]
+                ).grid(row = ele, column = 1)
+            self.chooseButtons[ele] = ttk.Button(self.frame_header,
+                text = "Choose PDF",
+                command = lambda: self.open(self.chooseLabelVars[ele])
+                ).grid(row = ele, column = 0)
+            self.move_combine_button(ele + 1)
+
+            #x += 1
+
+    def move_combine_button(self, row):
+        self.clear_button.grid(row = row, column = 0)
+        self.combine_button.grid(row = row, column = 1)
+    
+    
+            
+
 
 def main():            
     
